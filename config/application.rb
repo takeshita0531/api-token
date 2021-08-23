b59e54fad7945ec8a5bd250cdaeed95242988352
takeshita0531 <matubara0531@ezweb.ne.jp>
2021-08-23 08:37:01 +0000

    deploy

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module ApiTokenAuthentication
+module TestApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
